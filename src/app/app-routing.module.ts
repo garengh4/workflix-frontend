d50c3d4cb501e5489d1e1ffa19df7c8b729f2a7e
garengh4 <garengh4@gmail.com>
2023-02-21 16:46:05 -0500

    create-account path in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CreateAccountComponent } from "./create-account/create-account.component";
 import { enterComponent } from "./enter/enter.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
@@ -12,6 +13,8 @@ const routes: Routes = [
   { path: 'enter/login', component:LoginComponent},
   { path: 'profile', component:ProfileComponent},
   { path: 'profile/home', component:HomeComponent},
+  { path: 'create-account', component:CreateAccountComponent},
+
 
 
 
