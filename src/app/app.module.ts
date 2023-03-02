274ad2b4f1d7262b98d93e79b37838fec1276806
Garen <garengh4@gmail.com>
2023-03-02 17:46:48 -0500

    login-html complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 const modules = [
   MatNativeDateModule,
   MatFormFieldModule,
   MatInputModule,
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule
 ];
 
 @NgModule({
