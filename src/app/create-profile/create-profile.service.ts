9f7d45499099922045174c9ecad09b9d43b8d065
lalit sehgal <lalitsehgal1607@gmail.com>
2023-03-14 17:01:16 -0600

    Create profile validation with a few other changes

--- src/app/create-profile/create-profile.service.ts
+++ src/app/create-profile/create-profile.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 import { Profile } from "src/assets/entites/Profile";
 import { environment } from "src/environments/environment";
 
@@ -14,7 +14,26 @@ export class CreateProfileService {
 
     public createProfile(profileEntry: Profile): Observable<Profile> {
         let url = environment.backendLoginAPI+"/login-api/login";
-        return this.http.post<Profile>(url, profileEntry, { headers: this.headers });
+        return this.http.post<Profile>(url, profileEntry, { headers: this.headers }).pipe(catchError(this.handleError));;
 
+    }
+    private handleError(err: HttpErrorResponse) {
+        console.log(err)
+        let errMsg:string='';
+        if (err.error instanceof Error) {   
+            errMsg=err.error.message;
+            console.log(errMsg)
+        }
+         else if(typeof err.error === 'string'){
+            errMsg = JSON.parse(err.error).errorMessage
+        }
+        else {
+           if(err.status==0){ 
+               errMsg="A connection to back end can not be established.";
+           }else{
+            errMsg = err.error.errorMessage;
+           }
+         }
+            return throwError(errMsg);
     }
 }
