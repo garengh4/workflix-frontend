51a590bab9fcfbcaf889da0d1284e06013d61cc1
Sharon Voon <sharon.voon@infosys.com>
2023-03-16 11:40:54 -0700

    added logout button on the profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,27 +9,32 @@ import { ProfileService } from './profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit{
-profiles: Profile[];
+  profiles: Profile[];
 
-constructor(private profileService: ProfileService, private router: Router){}
+  constructor(private profileService: ProfileService, private router: Router){}
+
+  ngOnInit(): void {
+    this.getProfileByEmail();
+  }
   
 
-setCurrentProfileId(profileId:string):void {
-  localStorage.setItem("currentProfileId",profileId);
-}
+  setCurrentProfileId(profileId:string):void {
+    localStorage.setItem("currentProfileId",profileId);
+  }
 
-ngOnInit(): void {
-    this.getProfileByEmail();
+  public logout():void {
+    localStorage.setItem("loginId","");
+    localStorage.setItem("isLoggedIn","false");
+    localStorage.setItem("currentProfileId",""); 
   }
 
   public getProfileByEmail() {
-        this.profileService.getProfilesByLoginId(localStorage.getItem('loginId')).subscribe({
-          next: data =>{
-            this.profiles =data;
-          }
-
-        }
-        )
+    this.profileService.getProfilesByLoginId(localStorage.getItem('loginId')).subscribe({
+      next: data =>{
+        this.profiles =data;
       }
     }
+    )
+  }
+}
 
