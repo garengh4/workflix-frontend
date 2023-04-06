34f6e2f9292fbbc7446b7ea3d989ccf600e9dcab
Gareth Sykes <garethcsykes@gmail.com>
2023-04-06 12:42:05 -0400

    forgot to add changes to auth interceptor

--- src/app/auth/auth.interceptors.ts
+++ src/app/auth/auth.interceptors.ts
@@ -10,8 +10,8 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let authReq = req;
     const token = localStorage.getItem("access_token");
-    // const overrideToken = localStorage.getItem('overrideToken');
-    if (token != null) {
+    const overrideToken = localStorage.getItem('overrideToken');
+    if (token != null && overrideToken != 'true') {
       authReq = req.clone({ 
         setHeaders:{
             'Authorization': 'Bearer '+token,
