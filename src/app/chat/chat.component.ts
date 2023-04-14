282d49644879b3153939080ada12486e36f7848e
Gareth Sykes <garethcsykes@gmail.com>
2023-04-14 17:41:41 -0400

    can now chat to ChatGPT

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ChatGptService } from './chat-gpt.service';
+
+export class textResponse{
+  sno:number=1;
+  text:string='';
+  response:any='';
+}
 
 @Component({
   selector: 'app-chat',
@@ -6,5 +14,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+
+  textList:textResponse[]=[{sno:1,text:'',response:''}];
+
+  openaiAPIKey:string;
+
+  constructor(private openaiService: ChatGptService) {}
+
+  setOpenaiAPIKey() {
+    localStorage.setItem('openaiAPIKey', this.openaiAPIKey);
+  }
+
+  generateText(data:textResponse) {
+    this.openaiService.generateText(data.text).then(text => {
+      data.response = text;
+      if(this.textList.length===data.sno){
+        this.textList.push({sno:1,text:'',response:''});
+      }
+    });
+  }
 
 }
